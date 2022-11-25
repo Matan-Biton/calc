397e882eaf788b91ac173f35cfcaf747d70ff7bf
Matan-Biton <Matan.bit97@gmail.com>
2022-11-25 10:03:16 +0200

    a few upgrades

--- TS/application.ts
+++ TS/application.ts
@@ -1,51 +1,47 @@
 function onStart() {
   // adding the onclick listeners to all the digits, the four basic operators and the dot
   for (const btn of Array.from(
-    document.querySelectorAll("button.digit, button.dot")
+    document.querySelectorAll("button.digit, button.dot, button.operator")
   )) {
+    const func = btn.classList.contains("operator")
+      ? operatorClicked
+      : operandParsing;
     btn.addEventListener("click", () => {
-      toScreen(btn.id);
-    });
-  }
-
-  for (const btn of Array.from(document.querySelectorAll("button.operator"))) {
-    btn.addEventListener("click", () => {
-      operatorClicked(btn.id);
+      func(btn.id);
     });
   }
 
   // if page is loaded after configuration then sets the style accordingly
-  const search = new URLSearchParams(window.location.search);
-  document.body.style.backgroundColor = search.get("colorpick");
+  const urlParams = new URLSearchParams(window.location.search);
+  document.body.style.backgroundColor = urlParams.get("colorpick");
 
-  document.body.style.fontFamily = search.get("font");
+  document.body.style.fontFamily = urlParams.get("font");
 
-  if (search.get("theme") == "dark") {
-    document.body.classList.add("dark");
-  } else {
-    document.body.classList.remove("dark");
-  }
+  urlParams.get("theme") == "dark"
+    ? document.body.classList.add("dark")
+    : document.body.classList.remove("dark");
 }
 
 function openLog() {
-  document.querySelector(".log").classList.toggle("hidden");
-  document.querySelector("#log").classList.toggle("btnActive");
+  byId("log").classList.toggle("hidden");
+  byId("logBtn").classList.toggle("btnActive");
 }
 
 function toMemory() {
   const newChild = document.createElement("div");
-  newChild.innerHTML = `${screenElement.innerText}<div>(=${firstOperand})</div>`;
+  newChild.innerHTML = `${screenElement.innerText}<div> =${firstOperand}</div>`;
   memory.prepend(newChild);
 }
 
 function openScientific() {
-  document.querySelector(".science").classList.toggle("hidden");
-  document.querySelector("#scientific").classList.toggle("btnActive");
+  byId("science").classList.toggle("hidden");
+  byId("scienceBtn").classList.toggle("btnActive");
+  isScienceOn = !isScienceOn;
 }
 
 function screenLight() {
   screenElement.classList.toggle("light");
-  document.querySelector("#themeHandler").classList.toggle("btnActive");
+  byId("screenLight").classList.toggle("btnActive");
 }
 
 function alertInfo() {
@@ -62,7 +58,7 @@ function back() {
   //deletes from screen and resets the last of [first operand, operator, second operand]
   screenElement.textContent = "";
   if ((curOperand || firstOperand) && !curOperator) {
-    clearScreen();
+    screenElement.innerText = "";
   } else if (curOperator && !curOperand) {
     curOperator = "";
     screenElement.textContent = firstOperand;
@@ -76,7 +72,6 @@ function clearScreen() {
   screenElement.textContent = "";
   memory.innerHTML = "";
   curOperand = firstOperand = curOperator = "";
-  () => (document.querySelector(".log").textContent = "");
 }
 
 //calling back a function that sets listeners to objects in the HTML post load
--- TS/calculator.ts
+++ TS/calculator.ts
@@ -5,65 +5,49 @@ const screenElement = byId("screen");
 const memory = byId("memory");
 
 function byId(id: string) {
-	return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 function qsa(selector: string) {
-	return document.querySelectorAll(selector);
+  return document.querySelectorAll(selector);
 }
 
-function toScreen(id: string) {
-  //accepts only a digit or a dot and updates the current operand, only accepts valid option.
-  //accepts only one zero at the beginning to allow for 0.rest and accepts only one use of a dot in a number.
-  if (!curOperand) {
-    if (firstOperand && !curOperator) {
-      clearScreen();
-    }
-    screenElement.textContent += id;
-    curOperand = id;
-  } else if (
-    !(id == "." && curOperand.includes(".")) &&
-    !(id == "0" && curOperand == "0")
+function operandParsing(id: string) {
+  //if a valid (x.. not valid, 00xyz also not valid) button pressed updates the currant operand
+  if (
+    (id == "." && curOperand.includes(".")) ||
+    (id == "0" && curOperand == "0")
   ) {
-    if (curOperand == "0") {
-      curOperand = id;
-      screenElement.textContent =
-        screenElement.textContent.slice(0, -1) + curOperand;
-    } else {
-      screenElement.textContent += id;
-      curOperand = curOperand ? curOperand + id : id;
-    }
+    return;
   }
+  if (!curOperand && !curOperator) {
+    byId("screen").innerText = "";
+  }
+  curOperand += id;
+  screenElement.textContent += id;
 }
 
 function calculate() {
-  //eval the equation when called given that there are two operands (if there are two operands there most be an operator)
   if (curOperand && firstOperand) {
-    const screen = screenElement.innerText;
-    firstOperand = eval(screen.replace("x", "*")).toString();
+    firstOperand = eval(screenElement.innerText.replace("x", "*")).toString();
     curOperator = curOperand = "";
-    toMemory();
     screenElement.textContent = firstOperand;
+    toMemory();
   }
 }
 
 function operatorClicked(id: string) {
-  //when there is no operand do nothing.
-  //in case of one operand and no operator it sets the operator and starts filling the other operand.
-  //when there are two operands (and operator) there is a call for calculate and then an update for the first operand and operator
-  if (curOperand && firstOperand) {
-    calculate();
-    curOperator = id;
-    screenElement.textContent = firstOperand + " " + curOperator + " ";
-  } else if (curOperand) {
-    if (!curOperator) {
-      firstOperand = curOperand;
-      curOperand = "";
-      curOperator = id;
-      screenElement.textContent += " " + curOperator + " ";
-    }
-  } else if (firstOperand) {
-    curOperator = id;
-    screenElement.textContent = firstOperand + " " + curOperator + " ";
+	//when able, calculates
+	//when there are two operands (and operator) there is a call for calculate and then an update for the first operand and operator
+  if (!screenElement.innerText) {
+    return
   }
+	if (curOperator) {
+		calculate();
+	} else if (curOperand) {
+    firstOperand = curOperand;
+		curOperand = "";
+	}
+  curOperator = id;
+  screenElement.textContent = firstOperand + " " + curOperator + " ";
 }
